93f4d39563a7d914bd1350195bb08a3a68a01d05
Fabiano <fabianoquirinos@outlook.com>
2024-09-06 18:37:34 -0300

    feat: Criando o JavaScript no LocalStorage

---
+++ Projeto/js/formulario.js
@@ -0,0 +1,8 @@
+function saveValues(event) {
+  event.preventDefault();
+
+  const name = document.querySelector("name");
+  const email = document.querySelector("email");
+  const password = document.querySelector("senha");
+  const confirmPassword = document.querySelector("#senha2");
+}
