731ca8a9782080e3d35fb3b8ab609a3911bf4a4b
Fabiano <fabianoquirinos@outlook.com>
2024-09-07 12:21:57 -0300

    refactor: Refatorando o código da página de formulário

--- Projeto/js/formulario.js
+++
@@ -1,8 +0,0 @@
-function saveValues(event) {
-  event.preventDefault();
-
-  const name = document.querySelector("name");
-  const email = document.querySelector("email");
-  const password = document.querySelector("senha");
-  const confirmPassword = document.querySelector("#senha2");
-}
