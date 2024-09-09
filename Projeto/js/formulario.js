116847e7e4e59b41cd596dcb623f86f275d1fba2
Fabiano <fabianoquirinos@outlook.com>
2024-09-08 21:43:02 -0300

    feat: Finalizando a Persistência de Dados no LocalStorage

--- Projeto/js/formulario.js
+++ Projeto/js/formulario.js
@@ -1,9 +1,13 @@
-const elemento = document.querySelector("conteudo-form");
-const name = document.getElementById("name").value;
-const email = document.getElementById("email").value;
-const senha = document.getElementById("senha").value;
+const elemento = document.querySelector("#conteudo-form");
 
-function submitForm() {
+function submitForm(event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+  const senha2 = document.getElementById("senha2").value;
+
   localStorage.setItem("name", name);
   localStorage.setItem("email", email);
   localStorage.setItem("senha", senha);
